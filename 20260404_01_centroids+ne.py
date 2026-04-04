03701e3f73f167e09cfaf9f184e8bae19e9af089
heyutian <165613660+heytian@users.noreply.github.com>
2026-04-04 19:32:35 -0400

    centroids clasp report + ne population city names processing -and- added dates to recent scripts

---
+++ 20260404_01_centroids+ne.py
@@ -0,0 +1,37 @@
+# 20260403 - Adding city names and population from Natural Earth data to clasp report centroid csv
+
+import pandas as pd
+from shapely import wkt
+from sklearn.neighbors import BallTree
+import numpy as np
+
+targets = pd.read_csv('datasource/clasp_report_379cities.csv') # replace with local path
+cities = pd.read_csv('datasource/ne_10m_populated_places.csv') # replace with local path
+
+def parse_wkt_point(wkt_str):
+    try:
+        geom = wkt.loads(wkt_str)
+        return geom.x, geom.y
+    except:
+        return None, None
+
+targets[['lon', 'lat']] = targets['Site Center WKT'].apply(
+    lambda x: pd.Series(parse_wkt_point(x))
+)
+
+ne_coords_rad = np.radians(cities[['LATITUDE', 'LONGITUDE']].values)
+tree = BallTree(ne_coords_rad, metric='haversine')
+
+target_coords_rad = np.radians(targets[['lat', 'lon']].values)
+_, idx = tree.query(target_coords_rad, k=1)
+flat = idx.flatten()
+
+targets['City'] = cities.iloc[flat]['NAMEASCII'].values
+targets['Country'] = cities.iloc[flat]['ADM0NAME'].values
+targets['Population'] = cities.iloc[flat]['POP_MAX'].values
+
+output_path = "./output/csv/"
+targets.to_csv(output_path+'clasp_report_379cities_plus_v2.csv', index=False)
+
+
+
--- merge-co2-em-conc.py
+++ merge-co2-em-conc.py
@@ -1,11 +1,13 @@
 
+# 20260402: merging Ahn et al 2025 emissions with OCO3 data for 54 cities
+
 import pandas as pd
 import numpy as np
 
 # These parquets are outputs from nc4-timezone-pop.ipynb in this same repo
-co2 = pd.read_parquet("co2_sam.parquet") # path to CO2 parquet
-sif = pd.read_parquet("sif_sam.parquet") # path to SIF parquet
-c40emissions = pd.read_csv("Ahn_etal_2025_c40_emissions.csv", sep=",") # path to CO2 emissions data
+co2 = pd.read_parquet("./datasource/co2_sam.parquet") # path to CO2 parquet
+sif = pd.read_parquet("./datasource/sif_sam.parquet") # path to SIF parquet
+c40emissions = pd.read_csv("./datasource/Ahn_etal_2025_c40_emissions.csv", sep=",") # path to CO2 emissions data
 
 co2["datetime"] = pd.to_datetime(co2["datetime"])
 sif["datetime"] = pd.to_datetime(sif["datetime"])
@@ -28,7 +30,7 @@ merged = co2_mean.merge(sif_mean, on="city", how="outer")
 emissions_cols = [
     "TargetName", "Country", "TargetRegion", "Population", "GDP [billion USD]",
     "Annual CO2 Emissions, OCO-3 [MtCO2 year-1]",
-    "Annual CO2 Emissions, OCO-3, 1-sig uncertainty [MtCO2 year-1]",
+    "Number of SAMs",
 ]
 merged = merged.merge(
     c40emissions[emissions_cols],
